025732d8f18259a21d666015a7cd2a21245ad3e0
gsiwatch <gaurish.siwatch@gmail.com>
2021-09-27 12:14:50 -0400

    event listener

--- tooltip.js
+++ tooltip.js
@@ -1,28 +1,28 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
+        this._tooltipcontainer;
     }
 
     connectedCallback() {
         const tooltipicon = document.createElement('span');
         tooltipicon.textContent = ' (?)';
+        // bind it just to make sure that this is referred to this class only
+        tooltipicon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipicon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         this.appendChild(tooltipicon);
+    }
+
+    // convention indicating that I am calling it inside this class
+    _showTooltip() {
+        this._tooltipcontainer = document.createElement('div');
+        this._tooltipcontainer.textContent = 'This is the tooltip text!';
+        this.appendChild(this._tooltipcontainer);
+    }
+    
+    _hideTooltip() {
+        this.removeChild(this._tooltipcontainer);
     }
 }
 
 customElements.define('demo-tooltip', Tooltip); // to define the custom element
-
-/**
- * webComponent lifeCycle::
- *      1. constructor() called when the element is created - it is good for basic initializing
- *             - it is wrong place to access the element as at this point the element is not added 
- *               to the DOM
- *      2. connectedCallback(): this is called when the custom element is attached to the DOM
- *          therefore this is the place for DOMInitializations so that we can now start accessing 
- *          the DOM
- *      3. disconnectedCallback(): will be called whenever we will detach the customElement 
- *          from the DOM
- *      4. attributeChangedCallback(): this will be good to listen to the changes to the attribute to
- *          our own element
- *          can be used to updating the data 
- */
