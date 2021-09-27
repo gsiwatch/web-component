7d2333ab316f1f215172eded4c8e62a7a23c0dbe
gsiwatch <gaurish.siwatch@gmail.com>
2021-09-27 14:25:27 -0400

    example to add attribute

--- tooltip.js
+++ tooltip.js
@@ -2,21 +2,22 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         this._tooltipcontainer;
+        this._tooltipText;
     }
 
     connectedCallback() {
         const tooltipicon = document.createElement('span');
+        this._tooltipText = this.getAttribute('text');
         tooltipicon.textContent = ' (?)';
-        // bind it just to make sure that this is referred to this class only
         tooltipicon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipicon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         this.appendChild(tooltipicon);
     }
 
-    // _ convention indicating that I am calling it inside this class
     _showTooltip() {
         this._tooltipcontainer = document.createElement('div');
-        this._tooltipcontainer.textContent = 'This is the tooltip text!';
+        this._tooltipcontainer.textContent = 
+            this._tooltipText ?? 'provide some text';
         this.appendChild(this._tooltipcontainer);
     }
     
